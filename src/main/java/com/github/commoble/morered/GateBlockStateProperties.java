9271b12dae1c61a49a6fd0f90a4a749b10f1b1f3
Commoble <josephbettendorff@yahoo.com>
2020-05-24 21:52:49 -0500

    Plates placed against other plate line up with them

--- src/main/java/com/github/commoble/morered/GateBlockStateProperties.java
+++ src/main/java/com/github/commoble/morered/GateBlockStateProperties.java
@@ -6,6 +6,7 @@ import net.minecraft.state.DirectionProperty;
 import net.minecraft.state.IntegerProperty;
 import net.minecraft.state.properties.BlockStateProperties;
 import net.minecraft.util.Direction;
+import net.minecraft.util.math.BlockPos;
 
 public class GateBlockStateProperties
 {
@@ -28,8 +29,18 @@ public class GateBlockStateProperties
 	
 	public static BlockState getStateForPlacedGatePlate(BlockState state, BlockItemUseContext context)
 	{
-		Direction attachmentDirection = context.getFace().getOpposite();
-		int rotationIndex = BlockStateUtil.getRotationIndexForPlacement(attachmentDirection, context.getNearestLookingDirections());
+		Direction faceOfAdjacentBlock = context.getFace();
+		// special case: if the player placed this block against the side of another plate block,
+		// then place this block facing the same way as that plate block
+		// otherwise, place it against the block that was right-clicked0
+		Direction directionTowardAdjacentBlock = faceOfAdjacentBlock.getOpposite();
+		BlockPos placePos = context.getPos();
+		BlockPos adjacentPos = placePos.offset(directionTowardAdjacentBlock);
+		BlockState adjacentState = context.getWorld().getBlockState(adjacentPos);
+		boolean placedAgainstPlate = TagWrappers.LOGIC_GATE_PLATES.contains(adjacentState.getBlock()) && adjacentState.has(GateBlockStateProperties.ATTACHMENT_DIRECTION);
+		
+		Direction attachmentDirection = placedAgainstPlate ? adjacentState.get(GateBlockStateProperties.ATTACHMENT_DIRECTION) : directionTowardAdjacentBlock;
+		int rotationIndex = BlockStateUtil.getRotationIndexForPlacement(attachmentDirection, Direction.getFacingDirections(context.getPlayer()));
 		if (state.has(ATTACHMENT_DIRECTION) && state.has(ROTATION))
 		{
 			return state.with(ATTACHMENT_DIRECTION, attachmentDirection).with(ROTATION, rotationIndex);
---
+++ src/main/java/com/github/commoble/morered/TagWrappers.java
@@ -0,0 +1,8 @@
+package com.github.commoble.morered;
+
+import net.minecraft.tags.BlockTags;
+
+public class TagWrappers
+{
+	public static final BlockTags.Wrapper LOGIC_GATE_PLATES = new BlockTags.Wrapper(MoreRed.getRL("logic_gate_plates")); 
+}
