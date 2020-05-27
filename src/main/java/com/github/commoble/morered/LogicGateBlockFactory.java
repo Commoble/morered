1d0572aeba87637db77cabb5e12f184e3259ae5e
Commoble <josephbettendorff@yahoo.com>
2020-05-26 23:33:14 -0500

    Reorganize stuff

--- src/main/java/com/github/commoble/morered/LogicGateBlockFactory.java
+++
@@ -1,9 +0,0 @@
-package com.github.commoble.morered;
-
-import net.minecraft.block.Block;
-
-@FunctionalInterface
-public interface LogicGateBlockFactory
-{	
-	public LogicGatePlateBlock makeBlock(LogicFunction function, Block.Properties properties);
-}
