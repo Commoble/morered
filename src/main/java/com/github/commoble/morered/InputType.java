bf9f7f2a41c5a81ade8eb7cfea4532cc19848509
Commoble <josephbettendorff@yahoo.com>
2020-05-26 19:17:08 -0500

    Added NAND gates

--- src/main/java/com/github/commoble/morered/InputType.java
+++ src/main/java/com/github/commoble/morered/InputType.java
@@ -12,6 +12,11 @@ public class InputType
 		this.inputs = inputs;
 		this.plateMaker = (properties, function) -> new LogicGatePlateBlock(function, properties)
 		{
+			// fillStateContainer in LogicGatePlateBlock needs to know which blockstate properties to use
+			// but fillStateContainer gets called in the superconstructor, before any information about
+			// our block is available.
+			// The only safe way to handle this (aside from just making subclasses) is this
+			// cursed closure class
 			@Override
 			public InputSide[] getInputSides()
 			{
