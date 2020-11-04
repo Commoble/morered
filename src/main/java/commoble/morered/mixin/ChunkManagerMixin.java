e15c787d64c52bbcf2a8477ee130e71e08461a4c
Commoble <josephbettendorff@yahoo.com>
2020-11-04 14:45:22 -0600

    Add mixin for ChunkManager

---
+++ src/main/java/commoble/morered/mixin/ChunkManagerMixin.java
@@ -0,0 +1,11 @@
+package commoble.morered.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.world.server.ChunkManager;
+
+@Mixin(ChunkManager.class)
+public class ChunkManagerMixin
+{
+
+}
