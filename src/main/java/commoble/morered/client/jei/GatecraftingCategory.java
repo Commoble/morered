b4061a3cc5516d8db8148d75636be5a865839eff
Commoble <josephbettendorff@yahoo.com>
2023-07-16 18:39:59 -0400

    Reenable JEI support for gatecrafting

--- src/main/java/commoble/morered/client/jei/GatecraftingCategory.java
+++ src/main/java/commoble/morered/client/jei/GatecraftingCategory.java
@@ -1,85 +1,92 @@
 package commoble.morered.client.jei;
 
-// TODO fix JEI
-public abstract class GatecraftingCategory //implements IRecipeCategory<GatecraftingRecipe>
+import commoble.morered.MoreRed;
+import commoble.morered.ObjectNames;
+import commoble.morered.gatecrafting_plinth.GatecraftingRecipe;
+import mezz.jei.api.constants.ModIds;
+import mezz.jei.api.constants.VanillaTypes;
+import mezz.jei.api.gui.builder.IRecipeLayoutBuilder;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.helpers.IGuiHelper;
+import mezz.jei.api.recipe.IFocusGroup;
+import mezz.jei.api.recipe.RecipeIngredientRole;
+import mezz.jei.api.recipe.RecipeType;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import net.minecraft.core.NonNullList;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.inventory.CraftingContainer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.crafting.Recipe;
+
+public class GatecraftingCategory implements IRecipeCategory<Recipe<CraftingContainer>>
 {
-//	public static final ResourceLocation ID = MoreRed.getModRL(ObjectNames.GATECRAFTING_RECIPE);
-//	public static final ResourceLocation JEI_RECIPE_TEXTURE = new ResourceLocation(ModIds.JEI_ID, "textures/gui/gui_vanilla.png");
-//	
-//	private final IDrawable background;
-//	private final IDrawable icon;
-//	private final String localizedName;
-//	
-//	public GatecraftingCategory(IGuiHelper guiHelper)
-//	{
-//		this.icon = guiHelper.createDrawableIngredient(new ItemStack(ItemRegistrar.GATECRAFTING_PLINTH.get()));
-//		this.background = guiHelper.createDrawable(JEI_RECIPE_TEXTURE, 0, 60, 116, 54);
-//		this.localizedName = I18n.get("gui.morered.category.gatecrafting");
-//	}
-//
-//	@Override
-//	public ResourceLocation getUid()
-//	{
-//		return ID;
-//	}
-//
-//	@Override
-//	public Class<? extends GatecraftingRecipe> getRecipeClass()
-//	{
-//		return GatecraftingRecipe.class;
-//	}
-//
-//	@Override
-//	public String getTitle()
-//	{
-//		return this.localizedName;
-//	}
-//
-//	@Override
-//	public IDrawable getBackground()
-//	{
-//		
-//		return this.background;
-//	}
-//
-//	@Override
-//	public IDrawable getIcon()
-//	{
-//		return this.icon;
-//	}
-//
-//	@Override
-//	public void setIngredients(GatecraftingRecipe recipe, IIngredients ingredients)
-//	{
-//		// currently, we only have JEI support for recipes with at most 9 items
-//		// JEI doesn't seem to allow for stretchable recipe boxes?
-//		// we could do it by cycling through the ingredients over time. Hmm.
-//		List<Ingredient> inputs = recipe.getIngredients();
-//		int maxItems = Math.min(inputs.size(), 9);
-//		ingredients.setInputIngredients(recipe.getIngredients().subList(0, maxItems));
-//		ingredients.setOutput(VanillaTypes.ITEM, recipe.getResultItem());
-//	}
-//
-//	@Override
-//	public void setRecipe(IRecipeLayout recipeLayout, GatecraftingRecipe recipe, IIngredients ingredients)
-//	{
+	public static final RecipeType<Recipe<CraftingContainer>> TYPE = RecipeType.create(MoreRed.MODID, ObjectNames.GATECRAFTING_RECIPE, GatecraftingRecipe.class);
+	public static final ResourceLocation JEI_RECIPE_TEXTURE = new ResourceLocation(ModIds.JEI_ID, "textures/gui/gui_vanilla.png");
+	public static final String TITLE = "gui.morered.category.gatecrafting";
+	
+	private final IDrawable background;
+	private final IDrawable icon;
+	
+	public GatecraftingCategory(IGuiHelper guiHelper)
+	{
+		this.icon = guiHelper.createDrawableIngredient(VanillaTypes.ITEM_STACK, new ItemStack(MoreRed.instance().gatecraftingPlinthBlock.get()));
+		this.background = guiHelper.createDrawable(JEI_RECIPE_TEXTURE, 0, 60, 116, 54);
+	}
+	
+	
+
+	@Override
+	public RecipeType<Recipe<CraftingContainer>> getRecipeType()
+	{
+		return TYPE;
+	}
+
+	@Override
+	public Component getTitle()
+	{
+		return Component.translatable(TITLE);
+	}
+
+	@Override
+	public IDrawable getBackground()
+	{
+		
+		return this.background;
+	}
+
+	@Override
+	public IDrawable getIcon()
+	{
+		return this.icon;
+	}
+
+	@Override
+	public void setRecipe(IRecipeLayoutBuilder recipeLayout, Recipe<CraftingContainer> recipe, IFocusGroup focuses)
+	{
 //		// TODO add JEI support for gatecrafting recipes with more than 9 ingredients
-//		IGuiItemStackGroup guiItemStacks = recipeLayout.getItemStacks();
-//		
-//		// output slot
-//		guiItemStacks.init(0, false, 94, 18);
-//		
-//		// input slots
-//		for (int row=0; row<3; row++)
-//		{
-//			for (int column=0; column<3; column++)
-//			{
-//				int inputID = row*3 + column + 1;
-//				guiItemStacks.init(inputID, true, column*18, row*18);
-//			}
-//		}
-//		
-//		guiItemStacks.set(ingredients);
-//	}
+		recipeLayout.setShapeless();
+		
+		// output slot
+		recipeLayout.addSlot(RecipeIngredientRole.OUTPUT, 94, 18)
+			.addItemStack(recipe.getResultItem());
+		
+		// input slots
+		NonNullList<Ingredient> ingredients = recipe.getIngredients();
+		int ingredientCount = ingredients.size();
+		for (int row=0; row<3; row++)
+		{
+			for (int column=0; column<3; column++)
+			{
+				int inputID = row*3 + column;
+				var slot = recipeLayout.addSlot(RecipeIngredientRole.INPUT, column*18, row*18);
+				if (inputID < ingredientCount)
+				{
+					slot.addIngredients(ingredients.get(inputID));
+				}
+			}
+		}
+	}
 
 }
--- src/main/java/commoble/morered/client/jei/JEIProxy.java
+++ src/main/java/commoble/morered/client/jei/JEIProxy.java
@@ -1,69 +1,89 @@
 package commoble.morered.client.jei;
 
-// TODO fix JEI
-//@JeiPlugin
-public class JEIProxy // implements IModPlugin
+import java.util.List;
+
+import org.jetbrains.annotations.Nullable;
+
+import com.google.common.collect.ImmutableList;
+
+import commoble.morered.MoreRed;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.registration.IRecipeCatalystRegistration;
+import mezz.jei.api.registration.IRecipeCategoryRegistration;
+import mezz.jei.api.registration.IRecipeRegistration;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.inventory.CraftingContainer;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeManager;
+
+@JeiPlugin
+public class JEIProxy implements IModPlugin
 {
-//	public static final ResourceLocation ID = new ResourceLocation(MoreRed.MODID, MoreRed.MODID);
-//	
-//	@Nullable
-//	private GatecraftingCategory gatecraftingCategory;
-//
-//	@Override
-//	public ResourceLocation getPluginUid()
-//	{
-//		return ID;
-//	}
-//
-//	/**
-//	 * Register the categories handled by this plugin. These are registered before
-//	 * recipes so they can be checked for validity.
-//	 */
-//	@Override
-//	public void registerCategories(IRecipeCategoryRegistration registration)
-//	{
-//		this.gatecraftingCategory = new GatecraftingCategory(registration.getJeiHelpers().getGuiHelper());
-//		registration.addRecipeCategories(this.gatecraftingCategory);
-//	}
-//
-//	/**
-//	 * Register modded recipes.
-//	 */
-//	@Override
-//	public void registerRecipes(IRecipeRegistration registration)
-//	{
-//		if (this.gatecraftingCategory == null)
-//		{
-//			throw new NullPointerException("More Red's Gatecrafting JEI category failed to register! Notify the More Red author for assistance https://github.com/Commoble/morered/issues");
-//		}
-//		
-//		registration.addRecipes(getGatecraftingRecipes(), GatecraftingCategory.ID);
-//	}
-//
-//	/**
-//	 * Register recipe catalysts. Recipe Catalysts are ingredients that are needed
-//	 * in order to craft other things. Vanilla examples of Recipe Catalysts are the
-//	 * Crafting Table and Furnace.
-//	 */
-//	@Override
-//	public void registerRecipeCatalysts(IRecipeCatalystRegistration registration)
-//	{
-//		registration.addRecipeCatalyst(new ItemStack(ItemRegistrar.GATECRAFTING_PLINTH.get()), GatecraftingCategory.ID);
-//	}
-//
-//	public static List<IRecipe<CraftingInventory>> getGatecraftingRecipes()
-//	{
-//		ClientWorld world = Minecraft.getInstance().level;
-//
-//		if (world != null)
-//		{
-//			RecipeManager manager = world.getRecipeManager();
-//			return RecipeRegistrar.getAllGatecraftingRecipes(manager);
-//		}
-//		else
-//		{
-//			return ImmutableList.of();
-//		}
-//	}
+	public static final ResourceLocation ID = new ResourceLocation(MoreRed.MODID, MoreRed.MODID);
+	
+	@Nullable
+	private GatecraftingCategory gatecraftingCategory;
+
+	@Override
+	public ResourceLocation getPluginUid()
+	{
+		return ID;
+	}
+
+	/**
+	 * Register the categories handled by this plugin. These are registered before
+	 * recipes so they can be checked for validity.
+	 */
+	@Override
+	public void registerCategories(IRecipeCategoryRegistration registration)
+	{
+		this.gatecraftingCategory = new GatecraftingCategory(registration.getJeiHelpers().getGuiHelper());
+		registration.addRecipeCategories(this.gatecraftingCategory);
+	}
+
+	/**
+	 * Register modded recipes.
+	 */
+	@Override
+	public void registerRecipes(IRecipeRegistration registration)
+	{
+		if (this.gatecraftingCategory == null)
+		{
+			throw new NullPointerException("More Red's Gatecrafting JEI category failed to register! Notify the More Red author for assistance https://github.com/Commoble/morered/issues");
+		}
+		
+		registration.addRecipes(GatecraftingCategory.TYPE, getGatecraftingRecipes());
+	}
+
+	/**
+	 * Register recipe catalysts. Recipe Catalysts are ingredients that are needed
+	 * in order to craft other things. Vanilla examples of Recipe Catalysts are the
+	 * Crafting Table and Furnace.
+	 */
+	@Override
+	public void registerRecipeCatalysts(IRecipeCatalystRegistration registration)
+	{
+		registration.addRecipeCatalyst(new ItemStack(MoreRed.instance().gatecraftingPlinthBlock.get()), GatecraftingCategory.TYPE);
+	}
+
+	public static List<Recipe<CraftingContainer>> getGatecraftingRecipes()
+	{
+		@SuppressWarnings("resource")
+		ClientLevel clientLevel = Minecraft.getInstance().level;
+
+		if (clientLevel != null)
+		{
+			RecipeManager manager = clientLevel.getRecipeManager();
+			return MoreRed.getAllGatecraftingRecipes(manager);
+		}
+		else
+		{
+			return ImmutableList.of();
+		}
+	}
 
 }
