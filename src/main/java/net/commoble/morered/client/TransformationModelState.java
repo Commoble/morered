e5dc1eb1ddd9d85ab8b8080ea88c10852b94d2e6
Commoble <josephbettendorff@yahoo.com>
2025-04-07 19:03:58 -0400

    Fix windcatcher model parts not transforming

--- src/main/java/net/commoble/morered/client/TransformationModelState.java
+++ src/main/java/net/commoble/morered/client/TransformationModelState.java
@@ -4,7 +4,11 @@ import com.mojang.math.Transformation;
 
 import net.minecraft.client.resources.model.ModelState;
 
-public record TransformationModelState(Transformation transform) implements ModelState
+public record TransformationModelState(Transformation transformation) implements ModelState
 {
-
+	@Override
+	public Transformation transformation()
+	{
+		return this.transformation;
+	}
 }
